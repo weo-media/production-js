ec3f274fbe3babd1fc41044d54e70c7f36a831ff
Chris Elliott <chris.e@weomedia.com>
2021-01-23 11:49:15 -0800

    checkbox functions working. 'no match' included

--- previeweo/filter-sort.js
+++ previeweo/filter-sort.js
@@ -36,16 +36,43 @@
       useCallback = preact.useCallback;
 
     const input = JSON.parse(document.querySelector('.TPfilter-sort-input').innerText);
-    const selected = "";
+    let selectedSites = [];
+    let selectedTags = [];
 
     // get tags from input and remove duplicates by creating a Set then converting it to an array
     const tags = [...new Set(input.map((site) => [...site.tags].map(tag => tag.trim())).reduce((acc, cur) => [...acc, ...cur]))];
+    console.log(tags);
 
     // filter button
     const FilterButton = (props) => {
+      toggle = event => {
+        selectedSites = [];                                   // clear selectedSites array
+        selectedTags.indexOf(event.target.name) > -1 ?        // is tag already selected?
+          selectedTags.splice(selectedTags.indexOf(event.target.name), 1) : // remove if yes
+          selectedTags.push(event.target.name);               // add if no
+
+        input.forEach((site) => {                             // check each site option
+          if (selectedTags.every(tag => site.tags.includes(tag))) { // if all the selected tags are in that site's tags,
+            selectedSites.push(site);                         // add that site to the selectedSites
+          }
+        });
+
+        // re-renders html
+        let selected = '';
+        if (selectedTags.length && !selectedSites.length) {
+          selected = "empty";
+        }
+        else {
+          selected = "selected";
+        }
+
+        render(html`<${FilterSort} selected="${selected}" />`, document.querySelector('.TPfilter-sort-output'));
+      }
+
       return html`
-      <button class="filter-button" name="${props.filterName}" active="${props.active}">${props.children}</button>
-    `;
+          <input type="checkbox" class="filter-button" name="${props.name}" active="${props.active}" onClick=${this.toggle} />
+          <label>${props.name}</label>
+        `;
     }
 
     // site card
@@ -59,11 +86,24 @@
 
     // filter sort component
     const FilterSort = (props) => {
-      const sites = !selected ? input.map((site, idx) => html`<${SiteCard} img="${site.img}" key="${idx}" />`) : selected.map((site, idx) => html`<${SiteCard} img="${site.img}" key="${idx}" />`);
+      console.log(`selected: ${props.selected}`);
+      let sites = '';
+      // check "selectedSites" array for entries. if no entries, use "input" array to display all site cards else display site cards in "selectedSites" array
+      if (props.selected == "none") {
+        sites = input.map((site, idx) => html`<${SiteCard} img="${site.img}" key="${idx}" />`);
+      }
+      else if (props.selected == "selected") {
+        sites = selectedSites.map((site, idx) => html`<${SiteCard} img="${site.img}" key="${idx}" />`);
+      }
+      else if (props.selected == "empty") {
+        sites = "No matches were found";
+      }
+
+      const filterButtons = tags.map((tag) => html`<${FilterButton} name="${tag}" ></${FilterButton}`);
 
       return html`
     <div class="filterSort">
-      <div class="filterButtons"></div>
+      <div class="filterButtons">${filterButtons}</div>
       <div class="filteredSites">
         ${sites}
       </div>
@@ -72,6 +112,6 @@
     };
 
     // Renders html
-    render(html`<${FilterSort} />`, document.querySelector('.TPfilter-sort-output'));
+    render(html`<${FilterSort} selected="none" />`, document.querySelector('.TPfilter-sort-output'));
   }
 })();
